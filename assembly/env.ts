d6c589b5e43e151214fe1ebc7a26c2327b2b8db4
Jean-François Morcillo <jfm@massa.net>
2023-06-28 10:13:09 +0200

    Sync with massa-proto-as after proto changes
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>

--- assembly/env.ts
+++ assembly/env.ts
@@ -1,27 +1,11 @@
-import { Slot, decodeAbiResponse, encodeGenerateEventRequest, encodeTransferCoinsRequest } from 'massa-proto-as/assembly';
+import * as proto from "massa-proto-as/assembly";
+import { StringValue } from "massa-proto-as/assembly/google/protobuf/StringValue";
+import { decimalCount32 } from "util/number";
 
-import { NativeAddress } from 'massa-proto-as/assembly';
-import { NativeAmount } from 'massa-proto-as/assembly'
-import { NativeHash } from 'massa-proto-as/assembly'
-// import { CallResponse } from 'massa-proto-as/assembly'
-//import { CreateSCRequest } from 'massa-proto-as/assembly'
-//import { CreateSCResponse } from 'massa-proto-as/assembly'
-import { TransferCoinsRequest } from 'massa-proto-as/assembly'
-import { GenerateEventRequest } from 'massa-proto-as/assembly'
-import { decimalCount32 } from 'util/number'
-
-import { SetDataRequest, encodeSetDataRequest } from 'massa-proto-as/assembly';
-import { GetDataRequest, encodeGetDataRequest } from 'massa-proto-as/assembly';
-import { DeleteDataRequest, encodeDeleteDataRequest } from 'massa-proto-as/assembly';
-import { AppendDataRequest, encodeAppendDataRequest } from 'massa-proto-as/assembly';
-import { HasDataRequest, encodeHasDataRequest } from 'massa-proto-as/assembly';
-
-import { GetCurrentSlotRequest, encodeGetCurrentSlotRequest } from 'massa-proto-as/assembly';
-
-import { NativeHashRequest, encodeNativeHashRequest } from 'massa-proto-as/assembly';
-import { HashSha256Request, encodeHashSha256Request } from 'massa-proto-as/assembly';
-import { Keccak256Request, encodeKeccak256Request } from 'massa-proto-as/assembly';
-
+// ***************************************************************************
+// abi declarations
+// ***************************************************************************
+// /*
 // @ts-ignore: decorator
 @external("massa", "abi_set_data")
 declare function abi_set_data(arg: ArrayBuffer): ArrayBuffer;
@@ -75,213 +59,325 @@ declare function abi_hash_sha256(arg: ArrayBuffer): ArrayBuffer;
 declare function abi_hash_keccak256(arg: ArrayBuffer): ArrayBuffer;
 
 // @ts-ignore: decorator
-@external("massa", "abi_native_hash")
-declare function abi_native_hash(arg: ArrayBuffer): ArrayBuffer;
-
-/// performs a keccak256 hash on byte array and returns the hash as byte array
-export function hash_keccak256(data: Uint8Array): Uint8Array {
-    const req = new Keccak256Request(data);
-    const req_bytes = encodeKeccak256Request(req);
-    const resp_bytes = Uint8Array.wrap(abi_hash_keccak256(encode_length_prefixed(req_bytes).buffer));
-    const abi_resp = decodeAbiResponse(resp_bytes);
-    assert(abi_resp.error === null);
-    assert(abi_resp.res !== null);
-    assert(abi_resp.res!.keccak256Result !== null);
-    assert(abi_resp.res!.keccak256Result!.hash !== null);
-    return abi_resp.res!.keccak256Result!.hash
-}
-
-/// performs a sha256 hash on byte array and returns the hash as byte array
-export function hash_sha256(data: Uint8Array): Uint8Array {
-    const req = new HashSha256Request(data);
-    const req_bytes = encodeHashSha256Request(req);
-    const resp_bytes = Uint8Array.wrap(abi_hash_sha256(encode_length_prefixed(req_bytes).buffer));
-    const abi_resp = decodeAbiResponse(resp_bytes);
-    assert(abi_resp.error === null);
-    assert(abi_resp.res !== null);
-    assert(abi_resp.res!.hashSha256Result !== null);
-    assert(abi_resp.res!.hashSha256Result!.hash !== null);
-    return abi_resp.res!.hashSha256Result!.hash
-}
-
-/// performs a hash on byte array and returns the NativeHash
-export function native_hash(data: Uint8Array): NativeHash {
-    const req = new NativeHashRequest(data);
-    const req_bytes = encodeNativeHashRequest(req);
-    const resp_bytes = Uint8Array.wrap(abi_native_hash(encode_length_prefixed(req_bytes).buffer));
-    const abi_resp = decodeAbiResponse(resp_bytes);
-    assert(abi_resp.error === null);
-    assert(abi_resp.res !== null);
-    assert(abi_resp.res!.nativeHashResult !== null);
-    assert(abi_resp.res!.nativeHashResult!.hash !== null);
-    return assert(abi_resp.res!.nativeHashResult!.hash, "NativeHash computation failed");
-}
-
-/// gets the current execution slot
-export function get_current_slot(): Slot {
-    const req = new GetCurrentSlotRequest();
-    const req_bytes = encodeGetCurrentSlotRequest(req);
-    const resp_bytes = Uint8Array.wrap(abi_get_current_slot(encode_length_prefixed(req_bytes).buffer));
-    const resp = decodeAbiResponse(resp_bytes);
-    assert(resp.error === null);
-    assert(resp.res !== null);
-    assert(resp.res!.getCurrentSlotResult !== null);
-    assert(resp.res!.getCurrentSlotResult !== null);
-    return assert(resp.res!.getCurrentSlotResult!.slot, "Could not get current slot");
-}
+@external("massa", "abi_blake3_hash")
+declare function abi_blake3_hash(arg: ArrayBuffer): ArrayBuffer;
+// */
+// ***************************************************************************
+// utility functions
+// ***************************************************************************
 
 /// Creates a Uint8Array from an existing Uint8Array by prepending a little-endian i32 length prefix.
 export function encode_length_prefixed(data: Uint8Array): Uint8Array {
-    const len: i32 = data.length;
-    const result = new Uint8Array(4 + len);
-    result[0] = len & 0xff;
-    result[1] = (len >> 8) & 0xff;
-    result[2] = (len >> 16) & 0xff;
-    result[3] = (len >> 24) & 0xff;
-    result.set(data, 4);
-    return result;
+  const len: i32 = data.length;
+  const result = new Uint8Array(4 + len);
+  result[0] = len & 0xff;
+  result[1] = (len >> 8) & 0xff;
+  result[2] = (len >> 16) & 0xff;
+  result[3] = (len >> 24) & 0xff;
+  result.set(data, 4);
+  return result;
 }
 
 // abort() implementation adapted from https://github.com/AssemblyScript/wasi-shim.git
 export function myabort(
-    message: string | null,
-    fileName: string | null,
-    lineNumber: i32,
-    columnNumber: i32,
+  message: string | null,
+  fileName: string | null,
+  lineNumber: i32,
+  columnNumber: i32
 ): void {
-    // 0: len
-    // 4: buf...
-    const lenPtr: usize = 0
-    const bufPtr: usize = lenPtr + sizeof<usize>();
-    var ptr = bufPtr;
+  // 0: len
+  // 4: buf...
+  const lenPtr: usize = 0;
+  const bufPtr: usize = lenPtr + sizeof<usize>();
+  var ptr = bufPtr;
 
-    store<u64>(ptr, 0x203A74726F6261);
-    ptr += 7; // 'abort: '
+  store<u64>(ptr, 0x203a74726f6261);
+  ptr += 7; // 'abort: '
 
-    if (message != null) {
-        ptr += String.UTF8.encodeUnsafe(changetype<usize>(message), message.length, ptr);
-    }
-    store<u32>(ptr, 0x206E6920); ptr += 4; // ' in '
-    if (fileName != null) {
-        ptr += String.UTF8.encodeUnsafe(changetype<usize>(fileName), fileName.length, ptr);
-    }
+  if (message != null) {
+    ptr += String.UTF8.encodeUnsafe(
+      changetype<usize>(message),
+      message.length,
+      ptr
+    );
+  }
+  store<u32>(ptr, 0x206e6920);
+  ptr += 4; // ' in '
+  if (fileName != null) {
+    ptr += String.UTF8.encodeUnsafe(
+      changetype<usize>(fileName),
+      fileName.length,
+      ptr
+    );
+  }
 
-    store<u8>(ptr++, 0x28); // (
+  store<u8>(ptr++, 0x28); // (
 
-    var len = decimalCount32(lineNumber);
-    ptr += len;
-    do {
-        let t = lineNumber / 10;
-        store<u8>(--ptr, 0x30 + lineNumber % 10);
-        lineNumber = t;
-    } while (lineNumber); ptr += len;
+  var len = decimalCount32(lineNumber);
+  ptr += len;
+  do {
+    let t = lineNumber / 10;
+    store<u8>(--ptr, 0x30 + (lineNumber % 10));
+    lineNumber = t;
+  } while (lineNumber);
+  ptr += len;
 
-    store<u8>(ptr++, 0x3A); // :
+  store<u8>(ptr++, 0x3a); // :
 
-    len = decimalCount32(columnNumber);
-    ptr += len;
-    do {
-        let t = columnNumber / 10;
-        store<u8>(--ptr, 0x30 + columnNumber % 10);
-        columnNumber = t;
-    } while (columnNumber); ptr += len;
+  len = decimalCount32(columnNumber);
+  ptr += len;
+  do {
+    let t = columnNumber / 10;
+    store<u8>(--ptr, 0x30 + (columnNumber % 10));
+    columnNumber = t;
+  } while (columnNumber);
+  ptr += len;
 
-    store<u8>(ptr, 0x29); ptr++; // )
+  store<u8>(ptr, 0x29);
+  ptr++; // )
 
-    const msgLen = ptr - bufPtr;
-    store<u8>(lenPtr, msgLen & 0xff)
-    store<u8>(lenPtr + 1, (msgLen >> 8) & 0xff);
-    store<u8>(lenPtr + 2, (msgLen >> 16) & 0xff);
-    store<u8>(lenPtr + 3, (msgLen >> 24) & 0xff);
+  const msgLen = ptr - bufPtr;
+  store<u8>(lenPtr, msgLen & 0xff);
+  store<u8>(lenPtr + 1, (msgLen >> 8) & 0xff);
+  store<u8>(lenPtr + 2, (msgLen >> 16) & 0xff);
+  store<u8>(lenPtr + 3, (msgLen >> 24) & 0xff);
 
-    abi_abort(changetype<i32>(lenPtr));
+  abi_abort(changetype<i32>(lenPtr));
 
-    unreachable();
+  unreachable();
 }
 // end of abort() implementation
 
+export function stringToUint8Array(str: string): Uint8Array {
+  return Uint8Array.wrap(String.UTF8.encode(str));
+}
+
+function makeStringValue(
+  optional_sender_address: string | null
+): StringValue | null {
+  let sender_address: StringValue | null = null;
+
+  if (optional_sender_address !== null) {
+    sender_address = new StringValue(optional_sender_address);
+  }
+
+  return sender_address;
+}
+
+// ***************************************************************************
+// abi wrapper implementations
+// ***************************************************************************
+
 // ABI to call another SC
-// export function call(address: string, func_name: string, arg: Uint8Array, coins: u64): Uint8Array {
-//     const req = new CallRequest(new Address(address), func_name, arg, new Amount(coins));
-//     const req_bytes = Protobuf.encode(req, CallRequest.encode);
-//     const resp_bytes = Uint8Array.wrap(abi_call(encode_length_prefixed(req_bytes).buffer));
-//     const resp = Protobuf.decode<CallResponse>(resp_bytes, CallResponse.decode);
-//     return resp.returnData;
-// }
+export function call(
+  address: string,
+  func_name: string,
+  arg: Uint8Array,
+  coins: proto.NativeAmount
+): Uint8Array {
+  const req = new proto.CallRequest(address, func_name, arg, coins);
+  const req_bytes = proto.encodeCallRequest(req);
+  const resp_bytes = Uint8Array.wrap(
+    abi_call(encode_length_prefixed(req_bytes).buffer)
+  );
+  const resp = proto.decodeCallResponse(resp_bytes);
+  return resp.data;
+}
 
 // ABI to create a new SC
-// export function create_sc(bytecode: Uint8Array): string {
-//     const req = new CreateSCRequest(bytecode);
-//     const req_bytes: Uint8Array = Protobuf.encode(req, CreateSCRequest.encode);
-//     const resp_bytes = Uint8Array.wrap(abi_create_sc(encode_length_prefixed(req_bytes).buffer));
-//     const resp = Protobuf.decode<CreateSCResponse>(resp_bytes, CreateSCResponse.decode);
-//     if (resp.address === null) {
-//         // FIXME add fake args to please asc
-//         abort("Failed to create smart contract.", "", 0, 0);
-//     }
+export function create_sc(bytecode: Uint8Array): string {
+  const req = new proto.CreateScRequest(bytecode);
+  const req_bytes = proto.encodeCreateScRequest(req);
+  const resp_bytes = Uint8Array.wrap(
+    abi_create_sc(encode_length_prefixed(req_bytes).buffer)
+  );
+  const resp = proto.decodeAbiResponse(resp_bytes);
 
-//     // needed to please asc...
-//     const addr: Address = resp.address!;
-//     return addr.address;
+  assert(resp.error === null);
 
-// }
+  // const resp = Protobuf.decode<CreateScResponse>(resp_bytes, CreateSCResponse.decode);
+  if (resp.address === null) {
+    // FIXME add fake args to please asc
+    abort("Failed to create smart contract.", "", 0, 0);
+  }
+
+  const addr: string = resp.address!;
+  return addr.address;
+}
 
 // ABI to transfer coins to another address
-export function transfer_coins(senderAddress: NativeAddress | null, to_address: NativeAddress, coins: NativeAmount): void {
-    const req = new TransferCoinsRequest(senderAddress, to_address, coins);
-    const req_bytes = encodeTransferCoinsRequest(req);
-    abi_transfer_coins(encode_length_prefixed(req_bytes).buffer);
+export function transfer_coins(
+  to_address: string,
+  coins: proto.NativeAmount,
+  optional_sender_address: string | null
+): void {
+  const req = new proto.TransferCoinsRequest(
+    to_address,
+    coins,
+    makeStringValue(optional_sender_address)
+  );
+  const req_bytes = proto.encodeTransferCoinsRequest(req);
+  const resp_bytes = Uint8Array.wrap(
+    abi_transfer_coins(encode_length_prefixed(req_bytes).buffer)
+  );
+  const resp = proto.decodeAbiResponse(resp_bytes);
+
+  assert(resp.error === null);
 }
 
 // ABI to generate an event
-export function generate_event(event: string): void {
-    const req = new GenerateEventRequest(event);
-    const req_bytes = encodeGenerateEventRequest(req);
-    abi_generate_event(encode_length_prefixed(req_bytes).buffer);
+export function generate_event(event: Uint8Array): void {
+  const req = new proto.GenerateEventRequest(event);
+  const req_bytes = proto.encodeGenerateEventRequest(req);
+  const resp_bytes = Uint8Array.wrap(
+    abi_generate_event(encode_length_prefixed(req_bytes).buffer)
+  );
+
+  const resp = proto.decodeAbiResponse(resp_bytes);
+
+  assert(resp.error === null);
 }
 
-export function set_data(address: NativeAddress | null, key: Uint8Array, data: Uint8Array): void {
-    const req = new SetDataRequest(address, key, data);
-    const req_bytes = encodeSetDataRequest(req);
-    abi_set_data(encode_length_prefixed(req_bytes).buffer);
+export function set_data(
+  key: Uint8Array,
+  data: Uint8Array,
+  optional_address: string | null
+): void {
+  const req = new proto.SetDataRequest(
+    key,
+    data,
+    makeStringValue(optional_address)
+  );
+  const req_bytes = proto.encodeSetDataRequest(req);
+  abi_set_data(encode_length_prefixed(req_bytes).buffer);
 }
 
-export function get_data(address: NativeAddress | null, key: Uint8Array): Uint8Array {
-    const req = new GetDataRequest(address, key);
-    const req_bytes = encodeGetDataRequest(req);
-    const resp_bytes = Uint8Array.wrap(abi_get_data(encode_length_prefixed(req_bytes).buffer));
+export function get_data(
+  key: Uint8Array,
+  optional_address: string | null
+): Uint8Array {
+  const req = new proto.GetDataRequest(key, makeStringValue(optional_address));
+  const req_bytes = proto.encodeGetDataRequest(req);
+  const resp_bytes = Uint8Array.wrap(
+    abi_get_data(encode_length_prefixed(req_bytes).buffer)
+  );
 
-    const resp = decodeAbiResponse(resp_bytes);
+  const resp = proto.decodeAbiResponse(resp_bytes);
 
-    assert(resp.error === null);
-    assert(resp.res !== null);
-    assert(resp.res!.getDataResult !== null)
+  assert(resp.error === null);
+  assert(resp.res !== null);
+  assert(resp.res!.getDataResult !== null);
 
-    return resp.res!.getDataResult!.value;
+  return resp.res!.getDataResult!.value;
 }
 
-export function delete_data(address: NativeAddress | null, key: Uint8Array): void {
-    const req = new DeleteDataRequest(address, key);
-    const req_bytes = encodeDeleteDataRequest(req);
-    abi_delete_data(encode_length_prefixed(req_bytes).buffer);
+export function delete_data(
+  key: Uint8Array,
+  optional_address: string | null
+): void {
+  const req = new proto.DeleteDataRequest(
+    key,
+    makeStringValue(optional_address)
+  );
+  const req_bytes = proto.encodeDeleteDataRequest(req);
+  abi_delete_data(encode_length_prefixed(req_bytes).buffer);
 }
 
-export function append_data(address: NativeAddress | null, key: Uint8Array, data: Uint8Array): void {
-    const req = new AppendDataRequest(address, key, data);
-    const req_bytes = encodeAppendDataRequest(req);
-    abi_append_data(encode_length_prefixed(req_bytes).buffer);
+export function append_data(
+  key: Uint8Array,
+  data: Uint8Array,
+  optional_address: string | null
+): void {
+  const req = new proto.AppendDataRequest(
+    key,
+    data,
+    makeStringValue(optional_address)
+  );
+  const req_bytes = proto.encodeAppendDataRequest(req);
+  abi_append_data(encode_length_prefixed(req_bytes).buffer);
 }
 
-export function has_data(address: NativeAddress | null, key: Uint8Array): bool {
-    const req = new HasDataRequest(address, key);
-    const req_bytes = encodeHasDataRequest(req);
-    const resp_bytes = Uint8Array.wrap(abi_has_data(encode_length_prefixed(req_bytes).buffer));
+export function has_data(
+  key: Uint8Array,
+  optional_address: string | null
+): bool {
+  const req = new proto.HasDataRequest(key, makeStringValue(optional_address));
+  const req_bytes = proto.encodeHasDataRequest(req);
+  const resp_bytes = Uint8Array.wrap(
+    abi_has_data(encode_length_prefixed(req_bytes).buffer)
+  );
 
-    const resp = decodeAbiResponse(resp_bytes);
+  const resp = proto.decodeAbiResponse(resp_bytes);
 
-    assert(resp.error === null);
-    assert(resp.res !== null);
-    assert(resp.res!.hasDataResult !== null);
+  assert(resp.error === null);
+  assert(resp.res !== null);
+  assert(resp.res!.hasDataResult !== null);
 
-    return resp.res!.hasDataResult!.hasData;
+  return resp.res!.hasDataResult!.hasData;
+}
+
+/// performs a keccak256 hash on byte array and returns the hash as byte array
+export function hash_keccak256(data: Uint8Array): Uint8Array {
+  const req = new proto.Keccak256Request(data);
+  const req_bytes = proto.encodeKeccak256Request(req);
+  const resp_bytes = Uint8Array.wrap(
+    abi_hash_keccak256(encode_length_prefixed(req_bytes).buffer)
+  );
+  const abi_resp = proto.decodeAbiResponse(resp_bytes);
+  assert(abi_resp.error === null);
+  assert(abi_resp.res !== null);
+  assert(abi_resp.res!.keccak256Result !== null);
+  assert(abi_resp.res!.keccak256Result!.hash !== null);
+  return abi_resp.res!.keccak256Result!.hash;
+}
+
+/// performs a sha256 hash on byte array and returns the hash as byte array
+export function hash_sha256(data: Uint8Array): Uint8Array {
+  const req = new proto.HashSha256Request(data);
+  const req_bytes = proto.encodeHashSha256Request(req);
+  const resp_bytes = Uint8Array.wrap(
+    abi_hash_sha256(encode_length_prefixed(req_bytes).buffer)
+  );
+  const abi_resp = proto.decodeAbiResponse(resp_bytes);
+  assert(abi_resp.error === null);
+  assert(abi_resp.res !== null);
+  assert(abi_resp.res!.hashSha256Result !== null);
+  assert(abi_resp.res!.hashSha256Result!.hash !== null);
+  return abi_resp.res!.hashSha256Result!.hash;
+}
+
+/// performs a hash on byte array and returns the NativeHash
+export function blake3_hash(data: Uint8Array): Uint8Array {
+  const req = new proto.Blake3HashRequest(data);
+  const req_bytes = proto.encodeBlake3HashRequest(req);
+  const resp_bytes = Uint8Array.wrap(
+    abi_blake3_hash(encode_length_prefixed(req_bytes).buffer)
+  );
+  const abi_resp = proto.decodeAbiResponse(resp_bytes);
+  assert(abi_resp.error === null);
+  assert(abi_resp.res !== null);
+  assert(abi_resp.res!.blake3HashResult !== null);
+  assert(abi_resp.res!.blake3HashResult!.hash !== null);
+  return assert(
+    abi_resp.res!.blake3HashResult!.hash,
+    "NativeHash computation failed"
+  );
+}
+
+/// gets the current execution slot
+export function get_current_slot(): proto.Slot {
+  const req = new proto.GetCurrentSlotRequest();
+  const req_bytes = proto.encodeGetCurrentSlotRequest(req);
+  const resp_bytes = Uint8Array.wrap(
+    abi_get_current_slot(encode_length_prefixed(req_bytes).buffer)
+  );
+  const resp = proto.decodeAbiResponse(resp_bytes);
+  assert(resp.error === null);
+  assert(resp.res !== null);
+  assert(resp.res!.getCurrentSlotResult !== null);
+  assert(resp.res!.getCurrentSlotResult !== null);
+  return assert(
+    resp.res!.getCurrentSlotResult!.slot,
+    "Could not get current slot"
+  );
 }
--- assembly/test_transfer_coins.ts
+++ assembly/test_transfer_coins.ts
@@ -1,34 +1,29 @@
-import { AddressCategory, NativeAddress, NativeAmount } from "massa-proto-as/assembly";
+import { AddressCategory, NativeAmount } from "massa-proto-as/assembly";
 import * as env from "./env";
 
 // using a global to prevent problem with GC
 let shared_mem: ArrayBuffer = new ArrayBuffer(0);
 
 export function __alloc(size: i32): ArrayBuffer {
-    // /!\ Can't trace here
-    // // env.log("allocating " + size.toString() + "bytes");
+  // /!\ Can't trace here
+  // // env.log("allocating " + size.toString() + "bytes");
 
-    shared_mem = new ArrayBuffer(size);
-    return shared_mem;
+  shared_mem = new ArrayBuffer(size);
+  return shared_mem;
 }
 
 export function main(_args: ArrayBuffer): ArrayBuffer {
-    assert(changetype<usize>(shared_mem) == changetype<usize>(_args));
+  assert(changetype<usize>(shared_mem) == changetype<usize>(_args));
 
-    // For test purposes, we create two fake addresses (won't work on the blockchain)
-    const sender_buf = new Uint8Array(4);
-    sender_buf[0] = 0x31; sender_buf[1] = 0x32; sender_buf[2] = 0x33; sender_buf[3] = 0x34;
-    const sender_address = new NativeAddress(AddressCategory.ADDRESS_CATEGORY_USER_ADDRESS, 0, sender_buf);
-    
-    const to_buf = new Uint8Array(4);
-    to_buf[0] = 0x35; to_buf[1] = 0x36; to_buf[2] = 0x37; to_buf[3] = 0x38;
-    const to_address = new NativeAddress(AddressCategory.ADDRESS_CATEGORY_USER_ADDRESS, 0, to_buf);
+  // For test purposes, we create two fake addresses (won't work on the blockchain)
 
-    const amount = new NativeAmount(100, 0);
+  const to_address = "abcd"
+  const amount = new NativeAmount(100, 0);
+  const optional_sender_address = "efgh";
 
-    // Call the abi
-    env.transfer_coins(sender_address, to_address, amount);
+  // Call the abi
+  env.transfer_coins(to_address, amount, optional_sender_address);
 
-    shared_mem = env.encode_length_prefixed(new Uint8Array(0)).buffer;
-    return shared_mem;
+  shared_mem = env.encode_length_prefixed(new Uint8Array(0)).buffer;
+  return shared_mem;
 }
