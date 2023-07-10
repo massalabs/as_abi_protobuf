3ea2d029ea0da7fff4940b76b060cb9f50600261
Jean-François Morcillo <jfm@massa.net>
2023-07-10 16:03:27 +0200

    Add assert test in release mode
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>

---
+++ assembly/test_assert_in_release.ts
@@ -0,0 +1,18 @@
+import * as env from "./env";
+
+// using a global to prevent problem with GC
+let shared_mem: ArrayBuffer = new ArrayBuffer(0);
+
+export function __alloc(size: i32): ArrayBuffer {
+  shared_mem = new ArrayBuffer(size);
+  return shared_mem;
+}
+
+export function main(_args: ArrayBuffer): ArrayBuffer {
+  assert(changetype<usize>(shared_mem) == changetype<usize>(_args));
+
+  assert(false, "expected assert");
+
+  shared_mem = env.encode_length_prefixed(new Uint8Array(0)).buffer;
+  return shared_mem;
+}
