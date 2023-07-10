473941ec9ab57559528d05df7b0f2976d51f68d1
Sydhds <sylvain.delhomme@gmail.com>
2023-07-10 16:06:36 +0200

    Add gc unit test sources (#24)
    * Add gc unit test sources
    
    * Remove return in test_gc_basic_op.ts
    
    ---------
    
    Co-authored-by: sydhds <sydhds@gmail.com>

---
+++ assembly/test_gc_abi_call_basic.ts
@@ -0,0 +1,22 @@
+import * as env from "./env";
+
+// using a global to prevent problem with GC
+let shared_mem: ArrayBuffer = new ArrayBuffer(0);
+
+export function __alloc(size: i32): ArrayBuffer {
+  // /!\ Can't trace here
+  // // env.log("allocating " + size.toString() + "bytes");
+
+  shared_mem = new ArrayBuffer(size);
+  return shared_mem;
+}
+
+export function main(_args: ArrayBuffer): ArrayBuffer {
+  assert(changetype<usize>(shared_mem) == changetype<usize>(_args));
+
+  env.generate_event("Call 1");
+  env.generate_event("Call 2");
+
+  shared_mem = env.encode_length_prefixed(new Uint8Array(0)).buffer;
+  return shared_mem;
+}
