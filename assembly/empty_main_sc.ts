cbed27c98b6a61170268071e75a193eea31c9a6f
Leo-Besancon <lb@massa.net>
2023-07-20 08:41:40 +0200

    Added empty_main_sc (used in gas_calibration)

---
+++ assembly/empty_main_sc.ts
@@ -0,0 +1,19 @@
+import * as env from "./env";
+
+// using a global to prevent problem with GC
+let shared_mem: ArrayBuffer = new ArrayBuffer(0);
+
+export function __alloc(size: i32): ArrayBuffer {
+  // /!\ Can't trace here
+  // // env.generate_event("allocating " + size.toString() + "bytes");
+
+  shared_mem = new ArrayBuffer(size);
+  return shared_mem;
+}
+
+export function main(_args: ArrayBuffer): ArrayBuffer {
+  assert(changetype<usize>(shared_mem) == changetype<usize>(_args));
+  
+  shared_mem = env.encode_length_prefixed(new Uint8Array(0)).buffer;
+  return shared_mem;
+}
